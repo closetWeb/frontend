4f5aad04893a589932bfe5218e2a6f015c2a45de
parksungrae <qazwsxez0806@gmail.com>
2022-05-27 23:55:48 +0900

    feat: style set

---
+++ styles/theme.ts
@@ -0,0 +1,52 @@
+const margins = {
+  sm: ".5rem",
+  base: "1rem",
+  lg: "2rem",
+  xl: "3rem",
+};
+  
+const paddings = {
+  sm: ".5rem",
+  base: "1rem",
+  lg: "2rem",
+  xl: "3rem",
+};
+
+const fonts = {
+  family: {
+    base: `'Noto Sans KR', sans-serif`,
+    title: `'Merriweather', serif`,
+  },
+  size: {
+    sm: "1.4rem",
+    base: "1.6rem",
+    lg: "2rem",
+    xl: "2.5rem",
+    title: "6rem",
+  },
+  weight: {
+    light: 100,
+    normal: 400,
+    bold: 700,
+  },
+};
+
+const colors = {
+  red: "#ff4d4d",
+  yellow: "#ffff4d",
+  blue: "#0099ff",
+};
+
+export const device = {
+  mobile: "425px",
+  tablet: "768px",
+  desktop: "1440px",
+};
+
+export const defalutTheme = {
+  margins,
+  paddings,
+  fonts,
+  colors
+};
+
